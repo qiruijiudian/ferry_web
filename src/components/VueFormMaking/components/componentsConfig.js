53f7b06b4f8dbc315769d3ab8c4a875e4ee6a110
Euler <liucongyu@cdqrmi.com>
2024-03-14 17:04:16 +0800

    change ip+port setting

--- src/components/VueFormMaking/components/componentsConfig.js
+++ src/components/VueFormMaking/components/componentsConfig.js
@@ -420,7 +420,7 @@ export const advanceComponents = [
       headers: {},
       // isQiniu: false,
       tip: '', // 提示说明
-      action: 'http://192.168.1.103:8002/api/v1/public/uploadFile',
+      action: 'http://order.cdqrmi.com:8002/api/v1/public/uploadFile',
       labelWidth: 100,
       labelWidthDisabled: false,
       labelWidthStatus: true,
